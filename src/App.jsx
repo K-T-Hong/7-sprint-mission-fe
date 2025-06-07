0159f277ce7c4c718e5018870da372df62fd46c7
T.hong <t.1021.hong@gmail.com>
2025-06-07 14:32:34 +0900

    코드리뷰 수정 + 스프린트 미션 6 프론트엔트 진행

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import "./App.css";
 import React from "react";
 import Footer from "./components/Footer.jsx";
 import Navbar from "./components/Navbar.jsx";
-import Mainpage from "./components/Mainpage.jsx";
+import MarketMainPage from "./components/MarketMainPage.jsx";
+import LandingPage from "./components/LandingPage.jsx";
+import { Route, Routes } from "react-router-dom";
+import MarketItemsPage from "./components/MarketItemsPage.jsx";
+import RegistrationPage from "./components/RegistrationPage.jsx";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Mainpage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/sightseeing" element={<MarketMainPage />} />
+        <Route path="/items" element={<MarketItemsPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+      </Routes>
       <Footer />
     </div>
   );
